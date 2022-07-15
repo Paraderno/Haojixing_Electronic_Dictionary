9803a5230a58076c01d8bbeb11e17cfe97ad9204
K Leo <95621659+Paraderno@users.noreply.github.com>
2022-07-15 11:25:40 +0800

    Add files via upload

--- admin.h
+++ admin.h
@@ -15,7 +15,6 @@
  * 管理员输入需要增添的单词、词性与中文释义,向链表中增加一个单词
  * @param dlList 链表头结点
  * @param En 英文单词
- * @param attribute 词性
  * @param Cn 中文释义
  */
 void Add(DoublyLinkList * dlList, char* En, char* Cn);
@@ -24,7 +23,6 @@ void Add(DoublyLinkList * dlList, char* En, char* Cn);
  * 管理员输入需要删除的单词，文件中对应的单词将被删除，若输入不存在的单词将会报错
  * @param dlList 链表头结点
  * @param En 英文单词
- * @param attribute 词性
  * @param Cn 中文释义
  */
 void Delete(DoublyLinkList * dlList, char* En,  char* Cn);
@@ -34,7 +32,6 @@ void Delete(DoublyLinkList * dlList, char* En,  char* Cn);
  * @param dlList 链表头结点
  * @param pos 待修改单词的下标
  * @param En 英文单词
- * @param attribute 词性
  * @param Cn 中文释义
  */
 void Modify(DoublyLinkList * dlList, int pos, char* En, char* Cn);
